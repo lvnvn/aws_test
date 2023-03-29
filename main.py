444b448e4a29add431febd26a4af847a76e48ae4
Дарья Горохова <dgorokhova@ivi.ru>
2023-03-29 22:23:31 +0400

    fix lambda

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def lambda_handler(event, context):
 	for k,v in event.items():
 		print(k, v)
 
-return {
-	'statusCode': 200,
-	'body': json.dumps('Hello from Lambda')
-}
+	return {
+		'statusCode': 200,
+		'body': json.dumps('Hello from Lambda')
+	}
