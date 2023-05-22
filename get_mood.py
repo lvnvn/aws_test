2f286bacac80d646d519dcaa5feebad9963d2c2f
Дарья Горохова <dgorokhova@ivi.ru>
2023-05-22 21:04:23 +0400

    unify response format for get commands

--- get_mood.py
+++ get_mood.py
@@ -1,36 +1,37 @@
 import boto3
 import datetime
 import json
+from boto3.dynamodb.conditions import Key
 
-client = boto3.client("dynamodb")
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('Mood')
 
-def get_mood(email):
-	entry = client.get_item(
-		TableName="Mood",
-		Key={'Email': email}
-	)
-	return {
-		"statusCode": 200,
-		"body": json.dumps(entry)
-	}
-
-def get_moods():
-	items = client.scan(TableName="Mood", ProjectionExpression="Mood, EntryTime")["Items"]
+def make_response(items):
 	result = {}
 	for i, item in enumerate(items):
 		mood = item.get("Mood")
 		entry_time = item.get("EntryTime")
 		result[i] = f"{mood} at {entry_time}"
+	return result
 
+def get_moods_for_user(email):
+	items = table.query(KeyConditionExpression=Key('Email').eq(email)).get('Items', [])
 	return {
 		"statusCode": 200,
-		"body": json.dumps(result)
+		"body": json.dumps(make_response(items))
+	}
+
+def get_moods():
+	items = table.scan(ProjectionExpression="Mood, EntryTime")["Items"]
+	return {
+		"statusCode": 200,
+		"body": json.dumps(make_response(items))
 	}
 
 def lambda_handler(event, context):
 	params = event.get("queryStringParameters") or {} #  queryStringParameters value can be None
 	if "email" in params:
-		return get_mood(params["email"])
+		return get_moods_for_user(params["email"])
 	else:
 		return get_moods()
 	return {
